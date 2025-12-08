2d190f8c0735f9a29c9ea1647b7369bcb2fa10cd
ImJustNon <Im_Just_Non@hotmail.com>
2025-12-08 22:30:29 +0700

    Fix : Run time job missed and add more update schedule

--- scheduler.py
+++ scheduler.py
@@ -80,9 +80,11 @@ def run_scan():
 
 
 # 🔥 วันละ 3 รอบ
-scheduler.add_job(run_scan, "cron", hour=8, minute=0)  #8:00 น.
-scheduler.add_job(run_scan, "cron", hour=14, minute=0) #14:00 น.
-scheduler.add_job(run_scan, "cron", hour=19, minute=0) #19:00 น.
+# misfire_grace_time=60 allows jobs to run up to 60 seconds late
+scheduler.add_job(run_scan, "cron", hour=8, minute=0, misfire_grace_time=120)  #8:00 น.
+scheduler.add_job(run_scan, "cron", hour=14, minute=0, misfire_grace_time=120) #14:00 น.
+scheduler.add_job(run_scan, "cron", hour=19, minute=0, misfire_grace_time=120) #19:00 น.
+scheduler.add_job(run_scan, "cron", hour=22, minute=27, misfire_grace_time=120) #22:21 น.
 
 if __name__ == "__main__":
     print("📅 MRT Scheduler started")
